1359436758f1792f3fe56536def13df154dc6d49
Nauman <nauman.irfan5@gmail.com>
2018-03-10 17:23:24 +0530

    changed mongo lab uri

--- server/db/indexDB.js
+++ server/db/indexDB.js
@@ -1,5 +1,5 @@
 const Mongoose = require('mongoose');
 Mongoose.Promise = global.Promise;
-Mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+Mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/todoapp');
 
 module.exports = { Mongoose };
